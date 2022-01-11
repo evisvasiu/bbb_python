a16542e1823ccd2c47d0a6e1f1e875e40a454c16
evisvasiu <45014223+evisvasiu@users.noreply.github.com>
2022-01-12 00:15:19 +0100

    Update dht.py
    Thanks to www.steves-internet-guide.com for the original code.

--- dht.py
+++ dht.py
@@ -1,76 +1,120 @@
-import time
-import paho.mqtt.client as mqtt # mosquitto.py is deprecated
-
-user = "user"
-password = "nodered"
-#!/usr/bin/python
-# Copyright (c) 2014 Adafruit Industries
-# Author: Tony DiCola
-
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
-
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
-import sys
+###demo code provided by steve cope at www.steves-internet-guide.com
+##email steve@copeconsulting.co.uk
+###Free to use for any purpose
+"""
+Client reconnect demo code
+"""
+import paho.mqtt.client as mqtt  #import the client
+import time,sys
 import Adafruit_DHT
-import time
-import paho.mqtt.client as mqtt # mosquitto.py is deprecated
+keep_alive=20
+def on_disconnect(client, userdata, flags, rc=0):
+        print("DisConnected flags"+"result code "+str(rc)+"client_id  ")
+        client.connected_flag=False
 
-user = "****"
-password = "******"
-mqttc = mqtt.Client("BBB")
-mqttc.username_pw_set(user, password=password)
-mqttc.connect("138.3.246.220",1883,60)
 
-#mqttc.subscribe("test/", 2) # <- pointless unless you include a subscribe callback
-mqttc.loop_start()        #start the loop
-
-while True:
-
-# Parse command line parameters.
-        sensor_args = { '11': Adafruit_DHT.DHT11,
-                        '22': Adafruit_DHT.DHT22,
-                        '2302': Adafruit_DHT.AM2302 }
-        if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
-            sensor = sensor_args[sys.argv[1]]
-            pin = sys.argv[2]
+def on_connect(client, userdata, flags, rc):
+        if rc==0:
+                print("connected OK Returned code=",rc)
+                client.connected_flag=True #Flag to indicate success
         else:
-            print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
-            print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
-            sys.exit(1)
+                print("Bad connection Returned code=",rc)
+                client.bad_connection_flag=True
+                sys.exit(1) #quit
+def on_log(client, userdata, level, buf):
+        print("log: ",buf)
+def on_message(client, userdata, message):
+        print("message received  "  ,str(message.payload.decode("utf-8")))
+QOS1=1
+QOS2=0
+CLEAN_SESSION=False
+user = "****"
+password = "*******"
+port=1883
+broker="138.3.246.220"
+#broker="iot.eclipse.org" #use cloud broker
+cname="BBB"
+client = mqtt.Client(cname)    #create new instance
+client.on_log=on_log #client logging
+mqtt.Client.connected_flag=False #create flags
+mqtt.Client.bad_connection_flag=False #
+mqtt.Client.retry_count=0 #
+client.on_connect=on_connect        #attach function to callback
+client.on_disconnect=on_disconnect
+run_main=False
+run_flag=True
+retry=0
+retry_limit=20
+retry_delay_fixed=5
+connected_once=False
+count=0
+stime=time.time()
+retry_delay=retry_delay_fixed
+try:
+
+        while run_flag: #outer loop
+            #print("in loop",client.connected_flag)
+            ### this does the work
+                client.loop(0.01)
+                sensor_args = { '11': Adafruit_DHT.DHT11,
+                                '22': Adafruit_DHT.DHT22,
+                                '2302': Adafruit_DHT.AM2302 }
+                if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
+                        sensor = sensor_args[sys.argv[1]]
+                        pin = sys.argv[2]
+                else:
+                        print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
+                        print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
+                        sys.exit(1)
 
         # Try to grab a sensor reading.  Use the read_retry method which will retry up
         # to 15 times to get a sensor reading (waiting 2 seconds between each retry).
-        humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+                humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
+                if humidity is not None and temperature is not None:
+                        print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
+                else:
+                        print('Failed to get reading. Try again!')
+                        sys.exit(1)
+                if client.connected_flag:     #this is main loop
+                        print("in Main Loop")
+                        msg="test message"+str(count)
+                        ret=client.publish("iot",('{0:0.1f}, {1:0.1f}'.format(temperature, humidity)))
+                        print("publish",ret)
+                        time.sleep(5)
+             #######
+            ###handles reconnect
+                rdelay=time.time()-stime
+
+                if not client.connected_flag and rdelay>retry_delay:
+                        print("rdelay= ",rdelay)
+                        try:
+                                retry+=1
+                                if connected_once:
+                                        print("Reconnecting attempt Number=",retry)
+                                else:
+                                        print("Connecting attempt Number=",retry )
+
+                                client.username_pw_set(user, password=password)
+                                client.connect(broker,port)
+                                while not client.connected_flag:
+                                        client.loop(0.01)
+                                        time.sleep(1) #wait for connection to complete
+                                        stime=time.time()
+                                        retry_delay=retry_delay_fixed
+                                connected_once=True
+                                retry=0 #reset
+                        except Exception as e:
+                                print("\nConnect failed : ",e)
+                                retry_delay=retry_delay*retry_delay
+                                if retry_delay>1200:
+                                        retry_delay=1200
+                                print("retry Interval =",retry_delay)
+                                if retry>retry_limit:
+                                        sys.exit(1)
+except KeyboardInterrupt:
+        print("interrrupted by keyboard")
+
+print("quitting")
+client.disconnect()
 
 
-        # Un-comment the line below to convert the temperature to Fahrenheit.
-        # temperature = temperature * 9/5.0 + 32
-
-        # Note that sometimes you won't get a reading and
-        # the results will be null (because Linux can't
-        # guarantee the timing of calls to read the sensor).
-        # If this happens try again!
-        if humidity is not None and temperature is not None:
-            print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temperature, humidity))
-        else:
-            print('Failed to get reading. Try again!')
-            sys.exit(1)
-
-        mqttc.publish("iot",('{0:0.1f}, {1:0.1f}'.format(temperature, humidity)))
-		time.sleep(60) # sleep for 10 seconds before next call
-
-mqttc.loop_forever(retry_first_connection=True)
